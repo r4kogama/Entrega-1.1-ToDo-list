a21c7319ac85bab70033427c1a4a562d0c340fa1
r4kogama <thecureomd@gmail.com>
2024-06-05 14:13:07 +0200

    methods of class task  add, search, remove

--- src/shared/model/task.ts
+++ src/shared/model/task.ts
@@ -17,6 +17,20 @@ export class Task <T>  implements Note <T> {
         this.state = status;
     }
     
-   
+    addTask(task:Note<T>): void {
+        this.tasks.push(task);
+    }
+    
+    searchTask(task: Note<T>): Note<T> | undefined{
+        let findTask: Note<T> | undefined = this.tasks.find( (ele: Note<T>) => ele.name === task.name);
+        return findTask;
+    }
+    
+    removeTask(task: Note<T>): void {
+        let indexTask: number = this.tasks.findIndex( (ele : Note<T>) => ele.name === task.name );
+        if(indexTask !== -1){
+            this.tasks.splice(indexTask, 1);
+        }
+    }
     
 }
